1ebc4964fbbdbdedd85d9a66a1010ad50bd91224
Samuel Kihika <summitros@gmail.com>
2026-03-20 12:08:39 +0300

    specify files to be included in the package

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule LiveThree.MixProject do
 
   defp package do
     [
-      files: ~w(lib assets mix.exs .formatter.exs README),
+      files: ~w(lib assets mix.exs .formatter.exs README*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/kihikasamuel/live_three"},
       source_url: "https://github.com/kihikasamuel/live_three"
